c46081a3b42794f1e95f643674daca92c586b9ca
Brendan Baird <brendan.b517@gmail.com>
2023-02-13 13:01:23 -0500

    commit tree 28402669fe551edbe4f9f35bbe5279861b9ef8f2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+  attr_accessor :l1, :l2, :l3, :sides
+  
+  def initialize l1, l2, l3
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+    @sides = [@l1, @l2, @l3]
+  end
+
+  def kind
+    if ((@l1 < @l2 + @l3) && (@l2 < @l1 + @l3) && (@l3 < @l1 + @l2))
+      if @sides.include? 0
+        raise TriangleError
+      else
+        if @sides.uniq.size == 1
+          :equilateral
+        elsif @sides.uniq.size == 2
+          :isosceles
+        elsif @sides.uniq.size == 3
+          :scalene
+        end
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
